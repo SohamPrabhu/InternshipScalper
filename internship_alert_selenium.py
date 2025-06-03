19b606f719867e54b4092dc6e049e3ceb6e80243
Your Name <sohamprabhu@Mac.fios-router.home>
2025-06-02 20:56:53 -0400

    Added in a close method to the class so that it can quit properly

--- internship_alert_selenium.py
+++ internship_alert_selenium.py
@@ -38,7 +38,8 @@ class InternshipScraper:
         logging.info(f"Connecting to Postgres at host={POSTGRES_HOST}, port={POSTGRES_PORT}, db={POSTGRES_DB}, user={POSTGRES_USER}")
         logging.info(f"Email notifications from {EMAIL_FROM} to {EMAIL_TO} via {EMAIL_SMTP_SERVER}:{EMAIL_SMTP_PORT}")
 
-
+    def close(self):
+        self.driver.quit()
 
 def get_pg_conn():
     return psycopg2.connect(host=POSTGRES_HOST, port=POSTGRES_PORT, dbname=POSTGRES_DB,user=POSTGRES_USER,password=POSTGRES_PASSWORD)
@@ -66,6 +67,13 @@ def main():
     with open(COMPANIES_JSON,"r", encoding="utf-8") as f:
         companies = json.load(f)
     scraper = InternshipScraper()
+    try:
+        while True:
+            logging.info("Starting Scrapping Cycle")
+    except KeyboardInterrupt:
+        logging.info("Shutdown requested. Exiting.")
+    finally:
+        scraper.close()
 
 if __name__ == "__main__":
     main()
